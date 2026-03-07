7a200d7670161ab0503e2924d406908bf091679d
พิพัฒน์ โชคโคกกรวด <s6804062612277@email.kmutnb.ac.th>
2026-03-07 17:56:54 +0700

    Commit>> lab10no3_2.c

--- lab10.c
+++ lab10.c
@@ -4,17 +4,17 @@ struct Books{
     int BookID; 
     char BookTitle[50]; 
 } ComputerBook; 
-void DisplayData(struct Books CBook); //คือ printf ข้อมูล struct books จากพารามิเตอร์ที่รับเข้ามา
+void DisplayData(struct Books *CBook); 
 
 int main() { 
     printf("Enter Book ID :");      
     scanf("%d",&ComputerBook.BookID); 
     printf("Enter Book Title :");   
     scanf("%s",&ComputerBook.BookTitle); 
-    DisplayData(ComputerBook); // ส่งข้อมูลไปยังฟังก์ชัน DisplayData พารามิเตอร์ ComputerBook
+    DisplayData(&ComputerBook);    // ส่งตำแหน่งที่อยู่ข้อมูลไปยังฟังก์ชัน DisplayData 
     return 0; 
 } 
-void DisplayData(struct Books CBook){ 
-    printf("Book ID : %d \n",CBook.BookID); 
-    printf("Book Title : %s \n",CBook.BookTitle); 
+void DisplayData(struct Books *CBook){ 
+    printf("Book ID : %d \n",(*CBook).BookID); //ชี้ไป computerbook แล้วค่อยดึง bookid ออกมา
+    printf("Book Title : %s \n",CBook->BookTitle); // เหมือนกัน
 } 
